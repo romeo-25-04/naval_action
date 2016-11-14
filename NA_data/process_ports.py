7172c1cc490c8b5d346417ea9f341a8d2575964d
Roman-Admin <rcapsamun@yahoo.com>
2016-11-14 20:06:41 +0100

    port coords

--- NA_data/process_ports.py
+++ NA_data/process_ports.py
@@ -10,14 +10,18 @@ class ProcessPorts:
             x = port['Position']['x']
             z = port['Position']['z']
 
-            carl_x, carl_z = -23400.0005230308, 0
-            pr_x, pr_z = -42400.0009477139, 0
+            carl_x, carl_z = -23400.0005230308, -12000.0002682209
+            pr_x, pr_z = -42400.0009477139, -22600.0005051494
             delta_x = carl_x - pr_x
+            delta_z = carl_z - pr_z
             delta_x_grad = 75.56 - 75.18
-            one_grad = delta_x/delta_x_grad
+            delta_z_grad = 20.22 - 20.75
 
-            x_grad = x/one_grad
-            z_grad = z/one_grad
+            one_grad_x = delta_x/delta_x_grad
+            one_grad_z = delta_z / delta_z_grad
+
+            x_grad = x/one_grad_x
+            z_grad = z/one_grad_z
             lon = 76.00 + x_grad
             lat = 20.00 - z_grad
             port_lat_lon.append({
